12289b7931417325531fbc41b9eeba118f2bae07
tuananh205 <ntanh.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-11 19:16:58 +0700

    them folder

---
+++ chuong_6/bt2_chuong6.py
@@ -0,0 +1,11 @@
+#Tính toán số học
+x=10
+y=5
+tong=x+y
+hieu=x-y
+tich =x*y
+thương=x/y
+print('tổng của hai số ', x, '+' , y, '=',tong)
+print('hiệu của hai số ', x, '-' , y, '=',hieu)
+print('tích của hai số ', x, '*' , y, '=',tich)
+print('thương của hai số ', x, '/' , y, '=',thương)
---
+++ chuong_6/bt3_chuong6.py
@@ -0,0 +1,8 @@
+ten_hang = "Sữa hộp Vinamilk"
+so_luong = 5
+don_gia = 25000
+tien_phai_tra = so_luong * don_gia 
+print("Tên hàng:", ten_hang)
+print("Số lượng:", so_luong)
+print("Đơn giá:", don_gia)
+print("Tiền phải trả:", tien_phai_tra)
