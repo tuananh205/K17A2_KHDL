2ab97d4d2bf7f0102ffd290319ad123335d8d0e3
tuananh205 <149301518+tuananh205@users.noreply.github.com>
2024-01-10 09:09:24 +0700

    them folder

---
+++ chuong_13/chuong13bai1.py
@@ -0,0 +1,8 @@
+def mo_tep_tin():
+    name_file = input('nhập tên tệp tin :')
+    try:
+        with open(name_file,'r',encoding = 'utf-8') as f:
+            print('nội dùng tệp tin là : \n',f.read())
+    except FileNotFoundError:
+        print('tệp tin k tồn tại ')
+mo_tep_tin()
